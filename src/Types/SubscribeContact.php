fa89578e68147ee922c89c7c71bee594f84c0dbe
Dmytro Kulyk <lnkvisitor.ts@gmail.com>
2018-07-18 20:39:10 +0300

    Update subscribe object.

--- src/Types/SubscribeContact.php
+++ src/Types/SubscribeContact.php
@@ -10,8 +10,8 @@ use ESputnik\ESObject;
  * Class SubscribeContact
  *
  * @property Contact  $contact
- * @property string   $dedupeOn
  * @property string[] $groups
+ * @property string   $formType
  * @link http://esputnik.com.ua/api/el_ns0_subscribeContact.html
  */
 class SubscribeContact extends ESObject
@@ -21,15 +21,20 @@ class SubscribeContact extends ESObject
      */
     protected $contact;
 
-    /**
-     * @var string
-     */
-    protected $dedupeOn;
+//    /**
+//     * @var string
+//     */
+//    protected $dedupeOn;
 
     /**
      * @var string[]
      */
     protected $groups = array();
+
+    /**
+     * @var string
+     */
+    protected $formType = '';
 
     /**
      * @param Contact $contact
@@ -39,24 +44,24 @@ class SubscribeContact extends ESObject
         $this->contact = $contact;
     }
 
-    /**
-     * @param string $dedupeOn
-     *
-     * @throws ESException
-     */
-    public function setDedupeOn($dedupeOn): void
-    {
-        static $values = array(
-            'email',
-            'sms'
-        );
-
-        if (!\in_array($dedupeOn, $values, true)) {
-            throw new ESException('Property type must be one of ' . implode(', ', $values) . ' values.');
-        }
-
-        $this->dedupeOn = $dedupeOn;
-    }
+//    /**
+//     * @param string $dedupeOn
+//     *
+//     * @throws ESException
+//     */
+//    public function setDedupeOn($dedupeOn): void
+//    {
+//        static $values = array(
+//            'email',
+//            'sms'
+//        );
+//
+//        if (!\in_array($dedupeOn, $values, true)) {
+//            throw new ESException('Property type must be one of ' . implode(', ', $values) . ' values.');
+//        }
+//
+//        $this->dedupeOn = $dedupeOn;
+//    }
 
     /**
      * @param string[]|Group[] $groups
@@ -69,5 +74,13 @@ class SubscribeContact extends ESObject
             }
             return (string)$group;
         }, $groups);
+    }
+
+    /**
+     * @param string $formType
+     */
+    public function setFromType(string $formType): void
+    {
+        $this->formType = $formType;
     }
 }
